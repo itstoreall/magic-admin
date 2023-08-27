f96793a12b2f8b15c795bdd6d571cd8e654aef25
Serhii Stanislav <serhiistanislav@gmail.com>
2023-08-27 15:33:30 +0300

    ADD | new styles

--- src/components/Dashboard/MasterAdmin/MasterPanel.tsx
+++ src/components/Dashboard/MasterAdmin/MasterPanel.tsx
@@ -1,12 +1,12 @@
 import { useState } from 'react';
-import { useGlobalContext } from '../../../context/GlobalContext';
+// import { useGlobalContext } from '../../../context/GlobalContext';
 import s from './MasterPanel.module.scss';
 import AddAuthor from './AddAuthor';
 
 const MasterAdminPanel = () => {
   const [isOpenForm, setIsOpenForm] = useState<boolean>(false);
 
-  const { theme } = useGlobalContext();
+  // const { theme } = useGlobalContext();
 
   // const formModalHandler = (value?: boolean) => {
   //   setIsOpenForm(value ? value : !isOpenForm);
@@ -16,10 +16,29 @@ const MasterAdminPanel = () => {
     <div className={`${s.masterPanel} ${s['dark']}`}>
       <h3 className={s.title}>Master panel</h3>
 
-      <button className={s.button} onClick={() => setIsOpenForm(!isOpenForm)}>
-        Add new author
-      </button>
-      {isOpenForm && <AddAuthor isOpenForm={isOpenForm} />}
+      <div className={s.formWrap}>
+        <div className={s.formBlock}>
+          <button
+            className={s.button}
+            onClick={() => setIsOpenForm(!isOpenForm)}
+          >
+            Add new author
+          </button>
+
+          {isOpenForm && <AddAuthor isOpenForm={isOpenForm} />}
+        </div>
+
+        {/* <div className={s.formBlock}>
+          <button
+            className={s.button}
+            onClick={() => setIsOpenForm(!isOpenForm)}
+          >
+            Add new author
+          </button>
+
+          {isOpenForm && <AddAuthor isOpenForm={isOpenForm} />}
+        </div> */}
+      </div>
     </div>
   );
 };
