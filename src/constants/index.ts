b12f765f8d40ea4fb771e23ed9facd5b62c51ec3
Serhii Stanislav <serhiistanislav@gmail.com>
2023-10-10 20:43:48 +0300

    EDIT | move ipfs to env

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,8 +4,6 @@ export const ARTICLE_HEADER_FIELDS_EDIT = '**_edit_article_header_fields';
 export const ARTICLE_ELEMENTS_EDIT = '**_edit_article_elements';
 
 export const DEFAULT_PUBLIC_IMAGE = '/defaultPublicImg.jpg';
-export const IPFS_WEB3_STORAGE_ASTRAIA = 'ipfs.dweb.link/astraia-image.jpg';
-export const IPFS_WEB3_STORAGE_HEALTHY = 'ipfs.w3s.link/healthy-image.jpg';
 export const IPFS_WEB3_STORAGE_DEFAULT_CID =
   'bafybeibyrbaeduroa4p46rf7qm34bcbkqpcxfpmyk5g6lviagksaxxb4uy';
 
@@ -27,8 +25,20 @@ export const MONTHS = [
 export const SITE_ASTRAIA = 'https://astraia.storeall.com.ua/';
 export const SITE_HEALTHY = 'https://healthy.storeall.com.ua/';
 
-const getIpfsUrl = (blog: string) =>
-  blog === 'astraia' ? IPFS_WEB3_STORAGE_ASTRAIA : IPFS_WEB3_STORAGE_HEALTHY;
+const getIpfsUrl = (blog: string) => {
+  const opts = process.env.REACT_APP_OPTIONS;
+  const ipfsLink = process.env.REACT_APP_IPFS_LINK;
+
+  if (opts) {
+    const blogs = opts.split(' ');
+    const blogOne = blogs[0];
+    const blogTwo = blogs[1];
+
+    return blog === blogOne
+      ? `${ipfsLink}/${blogOne}-image.jpg`
+      : `${ipfsLink}/${blogTwo}-image.jpg`;
+  }
+};
 
 const fns = {
   getIpfsUrl,
--- src/utils/setImageSrc.ts
+++ src/utils/setImageSrc.ts
@@ -1,7 +1,6 @@
 import fns, { DEFAULT_PUBLIC_IMAGE } from '../constants';
 
 const defaultImage = DEFAULT_PUBLIC_IMAGE;
-// const web3StorageIpfs = constants.IPFS_WEB3_STORAGE;
 
 const setImageSrc = (blog: string, cid: string) =>
   cid ? `https://${cid}.${fns.getIpfsUrl(blog)}` : defaultImage;
